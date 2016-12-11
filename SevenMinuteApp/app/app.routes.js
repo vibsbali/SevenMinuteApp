2fa8a8fc178ec2a3ab1b57de27eb11d3f44a969b
Vaibhav Bali <vibsbali@gmail.com>
2016-12-12 05:17:05 +1100

    init

---
+++ SevenMinuteApp/app/app.routes.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+(function () {
+    angular.module("app")
+        .config(function ($routeProvider) {
+            $routeProvider.when("/start",
+            {
+                templateUrl: "app/partials/start.html"
+            });
+            $routeProvider.when("/workout",
+            {
+                templateUrl: "app/7MinWorkout/workout.html",
+                controller: "WorkoutController"
+            });
+            $routeProvider.when("/finish",
+            {
+                templateUrl: "app/partials/finish.html"
+            });
+            $routeProvider.otherwise({
+                redirectTo: "/start"
+            });
+        });
+
+}())
